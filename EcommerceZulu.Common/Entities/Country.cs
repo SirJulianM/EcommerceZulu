1834efbbb859b616148fea734a1c50bddf844c7b
Freddie8606 <ing_julianmadrid@hotmail.com>
2020-08-18 18:49:03 -0500

    Improve countries CRUD

---
+++ EcommerceZulu.Common/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceZulu.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} must contained less than {1} characteres.")]
+        public string Name { get; set; }
+
+    }
+}
---
+++ EcommerceZulu.web/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using EcommerceZulu.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceZulu.web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+           .HasIndex(t => t.Name)
+           .IsUnique();
+
+        }
+    }
+}
