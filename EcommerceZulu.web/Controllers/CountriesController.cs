09fabb435c85a7dd11430e1d48361fb49f04aac5
Freddie8606 <ing_julianmadrid@hotmail.com>
2020-08-21 01:04:48 -0500

    New Departments for a country

--- EcommerceZulu.web/Controllers/CountriesController.cs
+++ EcommerceZulu.web/Controllers/CountriesController.cs
@@ -20,7 +20,7 @@ namespace EcommerceZulu.web.Controllers
         // GET: Countries
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Countries.ToListAsync());
+            return View(await _context.Countries.Include(c => c.Departments).ToListAsync());
         }
 
         // GET: Countries/Details/5
@@ -31,7 +31,8 @@ namespace EcommerceZulu.web.Controllers
                 return NotFound();
             }
 
-            var country = await _context.Countries
+            var country = await _context.Countries.Include(c => c.Departments).ThenInclude(d => d.Cities)
+
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (country == null)
             {
@@ -84,6 +85,22 @@ namespace EcommerceZulu.web.Controllers
         }
 
         // GET: Countries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Country country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, Country country)
@@ -121,42 +138,6 @@ namespace EcommerceZulu.web.Controllers
             return View(country);
         }
 
-
-        // POST: Countries/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Country country)
-        //{
-            //if (id != country.Id)
-            //{
-                //return NotFound();
-            //}
-
-            //if (ModelState.IsValid)
-            //{
-                //try
-                //{
-                    //_context.Update(country);
-                    //await _context.SaveChangesAsync();
-                //}
-                //catch (DbUpdateConcurrencyException)
-                //{
-                    //if (!CountryExists(country.Id))
-                    //{
-                        //return NotFound();
-                    //}
-                    //else
-                    //{
-                        //throw;
-                    //}
-                //}
-                //return RedirectToAction(nameof(Index));
-            //}
-            //return View(country);
-        //}
-
         // GET: Countries/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -177,9 +158,63 @@ namespace EcommerceZulu.web.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CountryExists(int id)
+        public async Task<IActionResult> AddDepartment(int? id)
         {
-            return _context.Countries.Any(e => e.Id == id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Country country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            Department model = new Department { IdCountry = country.Id };
+            return View(model);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddDepartment(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                Country country = await _context.Countries.Include(c => c.Departments).FirstOrDefaultAsync(c => c.Id == department.IdCountry);
+                if (country == null)
+                {
+                    return NotFound();
+                }
+
+                try
+                {
+                    department.Id = 0;
+                    country.Departments.Add(department);
+                    _context.Update(country);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction($"{nameof(Details)}/{country.Id}");
+
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "There are a record with the same name.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            return View(department);
+        }
+
     }
 }
